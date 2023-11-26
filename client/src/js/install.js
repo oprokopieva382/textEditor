4378658d79deeee55c35c04a97253ae52c74d819
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-11-25 23:00:06 -0600

    remove no needed comments in code

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,14 +1,12 @@
 const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener("beforeinstallprompt", (event) => {
   event.preventDefault();
   // Store the deferred prompt for later use
   deferredPrompt = event;
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener("click", async () => {
   if (deferredPrompt) {
     deferredPrompt.prompt();
@@ -23,7 +21,6 @@ butInstall.addEventListener("click", async () => {
   }
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {
     console.log("👍", "appinstalled", event);
 });
