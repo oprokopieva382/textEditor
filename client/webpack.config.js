129eae4d436f88d5b0aa59ed9c94db366fd904ca
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-11-25 20:08:29 -0600

    set up webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,22 +8,68 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        filename: "index.html",
+      }),
+      new WebpackPwaManifest({
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E",
+        description: "Take notes with Javascript syntax highlighting!",
+        start_url: "/",
+        theme_color: "#225ca3",
+        background_color: "#225ca3",
+        orientation: "portrait",
+        display: "standalone",
+        icons: [
+          {
+            src: path.resolve("./src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("icons"),
+          },
+          {
+            src: path.resolve("./src/images/logo.png"),
+            size: "512x512",
+            destination: path.join("icons"),
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "service-worker.js",
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/plugin-transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
