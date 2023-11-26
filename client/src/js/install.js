fcb570a7302a32f3b51f3f495f6a82fd60f1e6d7
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-11-25 22:58:56 -0600

    add logic to install.js but still need to check later

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,14 +1,29 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {
-      event.preventDefault();
-      
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  // Store the deferred prompt for later use
+  deferredPrompt = event;
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener("click", async () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+
+    const choiceResult = await deferredPrompt.userChoice;
+
+    choiceResult.outcome === "accepted"
+      ? console.log("User accepted the PWA installation")
+      : console.log("User declined the PWA installation");
+
+      deferredPrompt = null;
+  }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {
+    console.log("👍", "appinstalled", event);
+});
