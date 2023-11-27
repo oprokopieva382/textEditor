383bdca31a4cb9fe092c7bf234b664155a5c4093
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-11-27 17:49:53 -0600

    update code of install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,26 +2,27 @@ const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 window.addEventListener("beforeinstallprompt", (event) => {
-  event.preventDefault();
   // Store the deferred prompt for later use
-    deferredPrompt = event;
+  window.deferredPrompt = event;
+  // Removes the hidden class from the button.
+  butInstall.classList.toggle("hidden", false);
 });
 
 butInstall.addEventListener("click", async () => {
-  console.log("Install button click")
-  if (deferredPrompt) {
-    deferredPrompt.prompt();
+  const promptEvent = window.deferredPrompt;
 
-    const choiceResult = await deferredPrompt.userChoice;
-
-    choiceResult.outcome === "accepted"
-      ? console.log("User accepted the PWA installation")
-      : console.log("User declined the PWA installation");
-
-      deferredPrompt = null;
+  if (!promptEvent) {
+    return;
   }
+
+  window.promptEvent.prompt();
+
+  window.deferredPrompt = null;
+
+   butInstall.classList.toggle("hidden", true);
 });
 
 window.addEventListener("appinstalled", (event) => {
-    console.log("👍", "appinstalled", event);
+  console.log("👍", "appinstalled", event);
+   window.deferredPrompt = null;
 });
